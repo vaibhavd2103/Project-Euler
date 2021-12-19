813c101c73d23359723fe4cd703223915681e3f1
vaibhad2103 <bountydange@gmail.com>
2021-12-19 21:34:22 +0530

    Optimize

--- Problem1.py
+++ Problem1.py
@@ -6,11 +6,7 @@ sum = 0
 till = int(input("Enter the number up to which you want to find the sum ------> "))
 
 for i in range(till):
-    if i % 3 == 0 and i % 5 == 0:
-        sum = sum + i
-    elif i % 5 == 0:
-        sum = sum + i
-    elif i % 3 == 0:
+    if i % 3 == 0 or i % 5 == 0:
         sum = sum + i
 print(
     f'The sum of all numbers below {till} divisible by 3, 5 and both is ------> {sum}')
